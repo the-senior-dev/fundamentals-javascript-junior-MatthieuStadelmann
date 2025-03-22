25d06e410736cd2035122f379e6cf8d299ed8069
Matthieu Stadelmann <stadelmannmatthieu@gmail.com>
2025-03-22 10:25:09 +0100

    Add solution Junior JS

--- src/01_primitives/01_getDataType.js
+++ src/01_primitives/01_getDataType.js
@@ -5,7 +5,28 @@
  * @returns {string} - The data type of the variable.
  */
 function getDataType(variable) {
-  // Your code here
+  if (typeof variable === "undefined") {
+    return "undefined";
+  }
+  if (typeof variable === "string") {
+    return "string";
+  }
+  if (typeof variable === "number") {
+    return "number"
+  }
+  if (typeof variable === "symbol") {
+    return "symbol";
+  }
+  if (typeof variable === "bigint") {
+    return "bigint";
+  }
+
+  if (typeof variable === "boolean") {
+    return "boolean";
+  }
+  if (Array.isArray(variable) || typeof variable === "object" || !typeof variable) {
+    return "object";
+  }
 }
 
 module.exports = getDataType;
